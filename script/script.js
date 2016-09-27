fdd727e01eae62e8856167444be209d23511f28b
ekihlberg <Emmas@wlanguest87229.eduroam.hj.se>
2016-09-27 13:44:45 +0200

    bug fix for the play and pause on mobile view and a start of a JSONP request

--- script/script.js
+++ script/script.js
@@ -1,12 +1,18 @@
 var audio = document.getElementById('Audio');
-            var btn = document.getElementById('audio_button');
-            function checkSound(){
-                if (audio.paused && !audio.ended && audio.currentTime > 0 ){
-                    audio.play();
-                    btn.src ="img/mute.png";
-                }
-                else {
-                    audio.pause();
-                    btn.src ="img/audio.png";
-                }
+    var btn = document.getElementById('audio_button');
+      function checkSound(){
+        if (audio.paused && !audio.ended && audio.currentTime > 0 ){
+              audio.play();
+              btn.src ="img/mute.png";
+        }
+        else {
+              audio.pause();
+              btn.src ="img/audio.png";
+        }
+
             }
+audio.addEventListener("ended", function(){
+                audio.currentTime = 0;
+                console.log("ended");
+                audio.play();
+                 });
