79ba5d1af4b820b359e0f9abdb153fb3b5ad42bf
ekihlberg <Emmas@wlanguest87229.eduroam.hj.se>
2016-09-27 14:13:50 +0200

    added some changes to the folder

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+/*   the following code is adapted from the documentation of Open Weather   */
+function ParseWeaterAPI(){
+	var weather = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&appid=c26821b846657e49aa006f7729139197&callback=parseWeather';
+	JSONPRequest(weather);
+}
 var audio = document.getElementById('Audio');
     var btn = document.getElementById('audio_button');
       function checkSound(){
@@ -16,3 +21,10 @@ audio.addEventListener("ended", function(){
                 console.log("ended");
                 audio.play();
                  });
+
+  /* The following 5 lines of code was adapted from the lecture material of lecture 7 in Gustav Mäkinen's course Client Programming with Javascript - TKJK15  */
+function JSONPRequest(url) {
+     var s = document.createElement('script');
+     s.setAttribute('src', url);
+     document.getElementsByTagName('head')[0].appendChild(s);
+}
